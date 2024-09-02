b3902c63f017b7f04129a7879e8309c9358ea8c8
smatrauade <smatra@uade.edu.ar>
2024-09-02 12:05:42 -0300

    hecho Apartado admin

--- Login_Alumnos_2.py
+++ Login_Alumnos_2.py
@@ -14,6 +14,13 @@ ingreso_sistemas = [
     ["fabrisuccar@sistem.edu.ar","fabrisuccar01"],
     ["lautipadin@sistem.edu.ar","lautipadin01"]
                                 ]
+ingreso_profes = [
+    ["pepi@sistem.edu.ar", "pepito10"],
+    ["profe@sistem.edu.ar", "profeuade"],
+    ["uade@sistem.edu.ar",   "uade24"]
+]
+
+
 #Se recortan los nombres de los productos a un máximo de 8 caracteres.
 productos_recortados = [[mail[:10], contraseña[:10]] for mail, contraseña in ingreso_sistemas]#####Ver los recortes
 for mail, contraseña in ingreso_sistemas:
@@ -26,28 +33,44 @@ print("*" * 26)
 
 for mail, contraseña in productos_recortados:
     print(f"|{mail:^10}| |{contraseña:^10}|")
-
 print()
 
+profeuser = "User-Prof"
+contraprofe = "Contraseña"
+profesores = [[email[:10], contra[:10]] for email, contra in ingreso_profes]
+print(f"|{profeuser:^10}| |{contraprofe:^10}|")
+print("-" * 26)
+for email, contra in profesores:
+    print(f"|{email:^10}| |{contra:^10}|")
+
 # Proceso de Log-in
-user=str (input("Por favor, ingresar su mail de usuario de alumno, sin @sistem: "))  #######En la entrega final que sea con @sistem, los profesores no. Hacer segunda matriz para profesores
+
+
+#Verificar si la cuenta existe o no en el sistema
+user=str(input("Por favor, ingresar su mail de usuario de alumno, sin @sistem: "))  #######En la entrega final que sea con @sistem, los profesores no. Hacer segunda matriz para profesores
 #Concatenación de cadenas
 ma = "@sistem.edu.ar"
 username = user + ma
 password=str (input("Ingresar contraseña de usuario: "))
-
-#Verificar si la cuenta existe o no en el sistema
-flag=0
+flag=False
 cont=0
-while flag==0 and cont<5:
+while flag==False and cont<5:
     i=0
-    while len(ingreso_sistemas)>i and flag!=1:
+    while i<len(ingreso_sistemas) and flag!=True:
         if username==ingreso_sistemas[i][0] and password==ingreso_sistemas[i][1]:
             print("Ingreso correcto a la aplicación.")
-            flag=1
-        else:
-            i += 1
-    if flag==0: #En caso de no encontrar el usuario, vuelve a preguntar con un maximo de 5 intentos.
+            flag=True
+        i+=1
+
+    if flag!=True:
+        j = 0
+        while j<len(ingreso_profes):
+            if username==ingreso_profes[j][0] and password==ingreso_profes[j][1]:
+                print("Ingreso correcto al apartado ADMIN")
+                flag = True
+            j+=1
+            
+    if flag==False: #En caso de no encontrar el usuario, vuelve a preguntar con un maximo de 5 intentos.
         cont += 1
         if cont<5:
             print("No se pudo acceder a una cuenta ya existente, por favor volver a intentarlo.")
@@ -58,4 +81,5 @@ while flag==0 and cont<5:
             password=str (input("Ingresar contraseña de usuario: "))
         else:
             print("Numerosos intentos fallidos, reintentar nuevamente en unos minutos.")
-print(f"\nFin de proceso") ###########Usar expresiones regulares
+    print(f"\nFin de proceso") ###########Usar expresiones regulares
+
