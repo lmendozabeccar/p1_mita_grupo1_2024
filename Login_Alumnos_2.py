95b9331116c3c8de4955300e01a21fb50e79c2d7
nvera <nvera@uade.edu.ar>
2024-09-04 16:56:52 -0300

    Login Alumnos

--- Login_Alumnos_2.py
+++ Login_Alumnos_2.py
@@ -1,4 +1,3 @@
-from main import main, encabezado_calificaciones, encabezado_asistencias
 print()
 print("Bienvenido al sector de Ingreso de la aplicación.")
 
@@ -14,13 +13,7 @@ ingreso_sistemas = [
     ["lucaspagli@sistem.edu.ar","lucaspagli01"],
     ["fabrisuccar@sistem.edu.ar","fabrisuccar01"],
     ["lautipadin@sistem.edu.ar","lautipadin01"]
-                                ]
-'''<<<<<<< HEAD
-#Se recortan los nombres de los productos a un máximo de 8 caracteres.
-productos_recortados = [[mail[:10], contraseña[:10]] for mail, contraseña in ingreso_sistemas]#####Ver los recortes
-for mail, contraseña in ingreso_sistemas:
-    print([mail[:10], contraseña[:10]])'''
-
+]
 ingreso_profes = [
     ["pepi@sistem.edu.ar", "pepito10"],
     ["profe@sistem.edu.ar", "profeuade"],
@@ -57,51 +50,45 @@ def inicio():
     flag = True
     print(f"\n¡Bienvenidos a nuestra Gestion Academica!")
     while flag!=False:
-        inicio=int(input("Quieres Iniciar sesion?\n1 Si\n2 No\nElija un número: "))
+        inicio=int(input("Quieres Iniciar sesion?\n1 Si\n2 No, deseo registrarme\n3 Salir \nElija un número: "))
         if inicio==1:
             login()
             flag = False
         elif inicio == 2:
-            registero=int(input("Quieres registrarte?\n1 Si\n2 No\nElija un número: "))
-            if registero==1:
-                print("¡Registrate!")
-                registro(ingreso_sistemas)
+            print("¡Registrate!")
+            registro(ingreso_sistemas)
+            flag = False
+            inicio_2=int(input("Quieres Iniciar sesion ahora?\n1 Si\n2 No\nElija un número: "))
+            if inicio_2 == 1:
+                login()
                 flag = False
-                inicio_2=int(input("Quieres Iniciar sesion ahora?\n1 Si\n2 No\nElija un número: "))
-                if inicio_2 == 1:
-                    login()
-                    flag = False
-                elif inicio_2==2:
-                    print("Saliendo..")
-                    flag = False
-            elif registero==2:
+            elif inicio_2==2:
+                print("Saliendo..")
+                flag = False
+        elif inicio==3:
                 print("Saliendo..")
                 flag = False
         
 # Proceso de Log-in
 def login():
     #Verificar si la cuenta existe o no en el sistema
-    user=str(input("Por favor, ingresar su mail de usuario de alumno, sin @sistem: "))  #######En la entrega final que sea con @sistem, los profesores no. Hacer segunda matriz para profesores
-    #Concatenación de cadenas
-    ma = "@sistem.edu.ar"
-    username = user + ma
+    user=str(input("Por favor, ingresar su mail de usuario: "))     
     password=str (input("Ingresar contraseña de usuario: "))
     flag=False
     cont=0
     while flag==False and cont<5:
         i=0
         while i<len(ingreso_sistemas) and flag!=True:
-            if username==ingreso_sistemas[i][0] and password==ingreso_sistemas[i][1]:
-                print("Ingreso correcto a la aplicación.")
+            if user==ingreso_sistemas[i][0] and password==ingreso_sistemas[i][1]:
+                print("Ingreso correcto al apartado alumno.")
                 flag=True
             i+=1
         if flag!=True:
             j = 0
             while j<len(ingreso_profes):
-                if username==ingreso_profes[j][0] and password==ingreso_profes[j][1]:
-                    print("Ingreso correcto al apartado ADMIN")
+                if user==ingreso_profes[j][0] and password==ingreso_profes[j][1]:
+                    print("Ingreso correcto al apartado profesor.")
                     flag = True
-                    main(encabezado_calificaciones, encabezado_asistencias)
                 j+=1
                 
         if flag==False: #En caso de no encontrar el usuario, vuelve a preguntar con un maximo de 5 intentos.
@@ -109,28 +96,25 @@ def login():
             if cont<5:
                 print("No se pudo acceder a una cuenta ya existente, por favor volver a intentarlo.")
                 print()
-                user=str (input("Por favor, ingresar su mail de usuario de alumno, sin @sistem: ")) 
-                ma = "@sistem.edu.ar"
-                username = user + ma
+                user=str (input("Por favor, ingresar su mail de usuario de alumno: ")) 
                 password=str (input("Ingresar contraseña de usuario: "))
             else:
-                print("Numerosos intentos fallidos, reintentar nuevamente en unos minutos.")
+                print("Numerosos intentos fallidos, reintentar nuevamente.")
 def registro(lista):
     flag = 0
     while flag == 0:
         flag_registro =True
         user=str(input("ingresar su usuario nuevo, sin @sistem: "))
-        sis = "@sistem.edu.ar"
-        us = user + sis
+
         for fil in lista:
-            if us == fil[0]:
-                print(us, fil[0])
+            if user == fil[0]:
+                print(user, fil[0])
                 flag_registro = False
         if flag_registro == False:
             print("Ese usuario ya existe, intentelo de nuevo.")
         else:
             pas=str(input("Ingrese su contraseña: "))
-            lista.append([us, pas])
+            lista.append([user, pas])
             flag = 1
     return lista
 inicio()
