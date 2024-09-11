5d61419e2b98134d52a745f85d524f131aec89fb
nvera <nvera@uade.edu.ar>
2024-09-11 15:44:52 -0300

    Lo mismo que antes, modularizado

--- Menú_de_Inicio.py
+++ Menú_de_Inicio.py
@@ -1,4 +1,5 @@
 from main import profesores, estudiantes
+from Validaciones import validacion_2dig, validacion_3dig, validacionmail, validar_contraseña, validar_mayus_nombre, validar_num, cuenta_existente_login, cuenta_existente_register
 import re
 #Alumnos ya registrados en el sistema.
 ingreso_alumnos = [
@@ -41,82 +42,11 @@ print("-" * 26)
 for email, contra in profesores:
     print(f"|{email:^10}| |{contra:^10}|") ###APLICAR EXPRESIONES REGULARES 
 
-#Validacion letras.
-def validar_num (car):    
-    if car.isnumeric()==False:
-        print("Por favor, ingresar un número.")
-    return car.isnumeric()
-#Validacion de que los numeros esten bien.
-def validacion_2dig (texto):                 
-    patron = "[1-2]{1}"
-    while re.match(patron,texto) == None:
-        print("Por favor, ingresar un numero válido.")
-        return False
-def validacion_3dig (texto):                 
-    patron = "[1-3]{1}"
-    while re.match(patron,texto) == None:
-        print("Por favor, ingresar un numero válido.")
-        return False
-#Validación del Mail.
-def validacionmail (mail):
-    patron = "[a-zA-Z0-9]+@[sistem]+\.[edu]+\.[ar]"
-    busqarr = re.findall (patron, mail)
-    if busqarr == []:
-        print("Ingresar @sistem.edu.ar junto con el nombre de usuario.")
-        return False
 #Pedir usuario y contraseña
 def user ():
     username=str(input("Ingresar su mail de usuario: "))
     password=str (input("Ingresar contraseña: "))
     return username,password
-#Verificar si la cuenta existe o no en el sistema
-def cuenta_existente_login (list_alumnos,list_profesores,user,contraseña):
-    i=0
-    a=0
-    while i<len(list_alumnos):
-        if user==list_alumnos[i][0] and contraseña==list_alumnos[i][1]: 
-            print("Ingreso correcto al apartado alumnos.")
-            return 1,i
-        i+=1
-    j = 0
-    while j<len(list_profesores):
-        if user==list_profesores[j][0] and contraseña==list_profesores[j][1]:
-            print("Ingreso correcto al apartado profesores.")
-            return 2,j
-        j += 1
-    return 3,a
-#Verificar si existe un USUARIO registrado anteriormente.
-def cuenta_existente_register (listaalumnos,listaprofes,usuario):
-    i,j=0,0    
-    while i<len(listaalumnos):
-        if usuario==listaalumnos[i][0]: 
-            print("Usuario ya existente.")
-            return False
-        i += 1
-    while j<len(listaprofes):
-        if  usuario==listaprofes[j][0]:
-            print("Usuario ya existente.")
-            return False
-        j += 1        
-#Verificar mayúsculas correspondientes a la hora de ingresar nombre (registro). FALTA
-def validar_mayus_nombre (nombre):
-    patron = "^[A-Z][a-z]*\\s[A-Z][a-z]*$"
-    busqueda = re.findall (patron,nombre)
-    if busqueda == []:
-        print("Ingresar mayúsculas al comienzo del nombre y del apellido.")
-        return False
-#Verificar que la contraseña cumpla con requisitos. FALTA
-def validar_contraseña (contra):
-    if len(contra)<8:
-        print("Contraseña inválida, por favor ingresar al menos 8 caracteres.")
-        return False
-    if re.findall ("\\s",contra) != []:
-        print("Por favor, no ingresar espacios en blanco a la hora de ingresar su contraseña.")
-        return False        
-    if re.findall ("[A-Z]+[a-z]+[0-9]+",contra) == []:
-        print("Su contraseña no pudo ser validada. Por favor ingresar al menos una mayúscula, una minúscula y un número.)")
-        return False
-    return True
 #Menú de inicio.
 def menu_de_inicio():
     flag = True
