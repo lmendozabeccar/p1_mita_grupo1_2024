6cbc21b25604efee152c3a45e8a5b83d60d1c21a
lmendozabeccar <lmendozabeccar@uade.edu.ar>
2024-10-02 10:34:03 -0300

    arreglé los módulos

--- GESTION_ACADEMICA/CRUDS/crudProfesores.py
+++ GESTION_ACADEMICA/CRUDS/crudProfesores.py
@@ -1,6 +1,6 @@
 from VALIDACIONES.Validaciones import seguir_texto, validar_legajo, validar_num
 from MATRICES.matriz_calificaciones import mostrar_notas
-from MATRICES.Diccionario_Materias import actualizar_notas
+from MATRICES.diccionario_materias import actualizar_notas
 from MATRICES.matriz_alumnos import ingreso_alumnos
 
 #posicion = lambda legajo, lista: [i for i in range(len(lista)) if lista[i][0] == legajo] #Saco en que posicion de la lista está el respecivo legajo
