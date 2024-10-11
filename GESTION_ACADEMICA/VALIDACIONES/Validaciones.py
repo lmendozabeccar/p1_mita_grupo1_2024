b91357854d8867a0d03f68203e5e339fa0709236
lmendozabeccar <lmendozabeccar@uade.edu.ar>
2024-10-10 22:27:35 -0300

    Falta: Test unitarios y eliminar la materia que el alumno recursó

--- GESTION_ACADEMICA/VALIDACIONES/Validaciones.py
+++ GESTION_ACADEMICA/VALIDACIONES/Validaciones.py
@@ -3,30 +3,25 @@ import re
 def validar_mayus_nombre (nombre):
     busq = re.findall ("[0-9]+",nombre) 
     if len(busq) != 0:
-        print("Por favor, no ingresar números en su nombre y apellido.") 
-        print()
+        print("\nPor favor, no ingresar números en su nombre y apellido.\n") 
         return False
     busq2 = re.findall ("^[A-Z][a-z]*\\s[A-Z][a-z]*$",nombre)
     if len(busq2) == 0:
-        print("Ingresar mayúsculas al comienzo del nombre y del apellido.")
-        print()
+        print("\nIngresar mayúsculas al comienzo del nombre y del apellido.\n")
         return False
     
 #Verificar que la contraseña cumpla con requisitos. 
 def validar_contraseña (contra):
     if len(contra)<8: #Minimo ocho caracteres
-        print("Contraseña inválida, por favor ingresar al menos 8 caracteres.")
-        print()
+        print("\nContraseña inválida, por favor ingresar al menos 8 caracteres.\n")
         return False
     busq = re.findall ("\\s",contra)
     if len(busq) != 0: #Que no haya espacios en blanco
-        print("Por favor, no ingresar espacios en blanco a la hora de ingresar su contraseña.")
-        print()
+        print("\nPor favor, no ingresar espacios en blanco a la hora de ingresar su contraseña.\n")
         return False        
     busq2 = re.findall ("[A-Z]+[a-z]+[0-9]+",contra)
     if len(busq2) == 0: #Contraseña segura
-        print("Su contraseña no pudo ser validada. Por favor ingresar al menos una mayúscula, una minúscula y un número.)")
-        print()
+        print("\nSu contraseña no pudo ser validada. Por favor ingresar al menos una mayúscula, una minúscula y un número.\n")
         return False
     return True
 
@@ -35,8 +30,7 @@ def validar_contraseña (contra):
 #Validacion letras.
 def validar_num (car):    
     if car.isnumeric()==False: #Si la cadena tiene numeros, devuelve True
-        print("Por favor, ingresar un número.")
-        print()
+        print("\nPor favor, ingresar un número.\n")
     return car.isnumeric()
 
 #Validacion de que los numeros esten bien. Valida que la respuesta sea "1" o "2",etc respectivamente
@@ -51,8 +45,7 @@ def validacionmail (mail):
     patron = r"[a-zA-Z0-9]{3,}@sistem+\.edu+\.ar" # R String --> Para que tome las secuencias de escape bien (Daba un warning)
     busq = re.findall (patron, mail)
     if len(busq) == 0:
-        print('Ingresar @sistem.edu.ar junto con el nombre de usuario, con un mínimo de tres caracteres antes del "@"')
-        print()
+        print('\nIngresar @sistem.edu.ar junto con el nombre de usuario, con un mínimo de tres caracteres antes del "@"\n')
         return False
 #Verificar si la cuenta existe o no en el sistema
 def cuenta_existente_login (list_alumnos,list_profesores,user,contraseña):
@@ -60,15 +53,13 @@ def cuenta_existente_login (list_alumnos,list_profesores,user,contraseña):
     a=0
     while i<len(list_alumnos):
         if user==list_alumnos[i][1] and contraseña==list_alumnos[i][2]: 
-            print("Ingreso correcto al apartado alumnos.")
-            print()
+            print("\nIngreso correcto al apartado alumnos.\n")
             return 1, list_alumnos[i][0] 
         i+=1
     j = 0
     while j<len(list_profesores):
         if user==list_profesores[j][0] and contraseña==list_profesores[j][1]:
-            print("Ingreso correcto al apartado profesores.")
-            print()
+            print("\nIngreso correcto al apartado profesores.\n")
             return 2, list_profesores[j][0]
         j += 1
     return 3,a
@@ -78,14 +69,12 @@ def cuenta_existente_register (listaalumnos,listaprofes,usuario):
     i,j=0,0    
     while i<len(listaalumnos):
         if usuario==listaalumnos[i][1]: 
-            print("Usuario ya existente.")
-            print()
+            print("\nUsuario ya existente.\n")
             return False
         i += 1
     while j<len(listaprofes):
         if  usuario==listaprofes[j][0]:
-            print("Usuario ya existente.")
-            print()
+            print("\nUsuario ya existente.\n")
             return False
         j += 1    
 
@@ -93,9 +82,7 @@ def cuenta_existente_register (listaalumnos,listaprofes,usuario):
 def seguir_texto (text):
     patron = "^[1-2]$"
     while re.match(patron,text) == None:
-        print()
-        print("Por favor, ingresar un numero válido.")
-        print()
+        print("\nPor favor, ingresar un numero válido.\n")
         return False
 
 #Validacion legajo.
@@ -115,6 +102,5 @@ def validar_nota (car):
     patron = r"^(10(\.0)?|[0-9](\.\d)?)$" # ()? Quiere decir que es opcional que haya un punto y un 0, | es operador OR
     nota_valida= re.match(patron,car)
     if nota_valida == None:
-        print("Nota incorrecta, por favor ingresar una nota válida.")
-        print()
+        print("\nNota incorrecta, por favor ingresar una nota válida.\n")
         return False    
