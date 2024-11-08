90e8b53528218d1c8288dfac5ce41cf14cbb7699
nvera <nvera@uade.edu.ar>
2024-11-08 12:29:12 -0300

    Casi termino

--- GESTION_ACADEMICA/MATRICES/matriz_calificaciones.py
+++ GESTION_ACADEMICA/MATRICES/matriz_calificaciones.py
@@ -1,6 +1,7 @@
 import json
 from Base_de_datos.funciones_json import devolverjson, guardarjson
 from VALIDACIONES.Validaciones import validacion_dig
+from VALIDACIONES.test_main import aprobado_desaprobado
 
 # Se aplica recursividad en las funciones de suma() y contar() para luego aplicarlo como tupla en la funcion mostrar_notas()
 def suma(notas): 
@@ -53,30 +54,34 @@ def mostrar_notas (matriz_legajos_notas,legajo,tipo_usuario):
                 print(f"Cursa la materia {cursa} y obtuviste una nota de: {nota}")
             print(f"El promedio del alumno fue de: {sumas/contador:.2f}") #Maximo dos decimales
             if tipo_usuario == "alumno":
-                i=0
-                while len(sublista_cursa)>i:
-                    if sublista_notas[i]>= 0 and sublista_notas[i]<=3:
-                    
-                        print(f"Lamentablemente, debes recursar la materia",sublista_cursa[i])
+                materias= int
+                materias = 0
+                while len(sublista_cursa)>materias:
+                    try:
+                        assert (aprobado_desaprobado(sublista_notas[materias])) == True, "En caso que el alumno deba recursar"
+                    except AssertionError:  
+                                         
+                        print(f"Lamentablemente, debes recursar la materia",sublista_cursa[materias])
                         menu_recursa = "\n1 Desea cursar nuevamente la materia.\n2 Desea abandonar la materia. \nPor favor, elegir una opción: "
                         recursa = input(menu_recursa)
                         while validacion_dig (recursa, 2)==False: #Valida que sea un numero del 1 al 10
                             recursa = input(menu_recursa)
-                        indice = matriz_legajos_notas[legajo]['cursa'].index(sublista_cursa[i])
+                        indice = matriz_legajos_notas[legajo]['cursa'].index(sublista_cursa[materias])
                         if recursa == "1":
-                            print("Buena suerte para el próximo cuatrimestre.")
+                            print("\nBuena suerte para el próximo cuatrimestre.")
                             matriz_legajos_notas[legajo]["notas"][indice] = -1
                             guardarjson(matriz_legajos_notas)
                         elif recursa == "2":                            
                             matriz_legajos_notas[legajo]['cursa'].pop(indice)
                             matriz_legajos_notas[legajo]['notas'].pop(indice)
                             guardarjson(matriz_legajos_notas)
-                            print("Abandonaste la materia.")
-                            
-                            i -= 1                      
+                            print("\nAbandonaste la materia.")
+                            materias -= 1                      
                         else:
                                 print("No existe")
-                    i += 1
+                    print(materias)
+                    materias = materias + 1
+
     print("\nVolviendo al menú principal...")          
     return matriz_legajos_notas
 
---
+++ GESTION_ACADEMICA/VALIDACIONES/test_main.py
@@ -0,0 +1,5 @@
+def aprobado_desaprobado (notafinal):
+    if notafinal == -1:
+        return True
+    notaminima = 4
+    return notafinal >= notaminima    
